f6f8618faa1519fdca125b697a4a2b83c3c1865e
Tolga Zorlu <tolgazorlu@Tolga-Air.home>
2024-02-04 23:37:51 +0300

    without kafka

--- server/upload_image.go
+++ server/upload_image.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io"
+	"log"
+	"os"
 
 	pb "github.com/tolgazorlu/photo-analysis/proto"
 )
@@ -11,20 +13,20 @@ func (s *Server) UploadImage(ctx context.Context, in *pb.UploadImageRequest) (*p
 
 	// ! IF KAFKA DOESN'T WORK, COMMENT THE CODES BELOW
 
-	if err := AsyncSendToCloud(in.ImageName, in.ImageData); err != nil {
-		fmt.Printf("Error sending to vision api: %v\n", err)
-	}
+	// if err := AsyncSendToCloud(in.ImageName, in.ImageData); err != nil {
+	// 	fmt.Printf("Error sending to vision api: %v\n", err)
+	// }
 
 	serveFrames(in.ImageData, in.ImageName)
 
 	// ! IF KAFKA DOESN'T, USE THE CODES BELOW
 
-	// writer := io.Writer(os.Stdout)
+	writer := io.Writer(os.Stdout)
 
-	// err := detectFaces(writer, in.ImageName, in.ImageData)
-	// if err != nil {
-	// 	log.Println("Error detecting faces:", err)
-	// }
+	err := detectFaces(writer, in.ImageName, in.ImageData)
+	if err != nil {
+		log.Println("Error detecting faces:", err)
+	}
 
 	return &pb.UploadImageResponse{
 		ImageId: "0",
